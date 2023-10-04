a3dc5361ef572511cdec604f3e0184606c46cb52
abenezertesfaye <abenezertesfaye92@gmail.com>
2023-10-04 15:28:28 -0700

    added several pages

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
   const bar = document.querySelector('.bar');
   const navBar = document.getElementById('navbar');
   const close = document.querySelector('.close');
+//   const links = navBar.querySelectorAll('a');
 
   navBar.style.transform = 'translateX(-100%)';
 
-  bar.addEventListener("click", (e) => {
+  bar.addEventListener("click", () => {
     navBar.style.transform = 'translateX(0%)';
   });
 
   close.addEventListener("click", () => {
     navBar.style.transform = 'translateX(-100%)';
+  });
+
+  links.forEach((link) => {
+	link.addEventListener("click", (e) => {
+		navBar.style.transform = 'translateX(-100%)';
+	});
   })
-})
+// })
 
 
