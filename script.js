0e3c27a699bf9ba9a2d1eb05b4e8231071d6200b
abenezertesfaye <abenezertesfaye92@gmail.com>
2023-10-05 15:46:08 -0700

    finalized responsive design for the layout pages

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-// document.addEventListener("DOMContentLoaded", () => {
-  const bar = document.querySelector('.bar');
-  const navBar = document.getElementById('navbar');
-  const close = document.querySelector('.close');
-//   const links = navBar.querySelectorAll('a');
+const bar = document.querySelector('.bar');
+const navBar = document.getElementById('navbar');
+const close = document.querySelector('.close');
+const links = document.querySelectorAll('.link');
 
-  navBar.style.transform = 'translateX(-100%)';
+navBar.style.transform = 'translateX(-100%)';
 
-  bar.addEventListener("click", () => {
-    navBar.style.transform = 'translateX(0%)';
-  });
+bar.addEventListener("click", () => {
+	navBar.style.transform = 'translateX(0%)';
+});
 
-  close.addEventListener("click", () => {
-    navBar.style.transform = 'translateX(-100%)';
-  });
+close.addEventListener("click", () => {
+	navBar.style.transform = 'translateX(-100%)';
+});
 
-  links.forEach((link) => {
+links.forEach(link => {
 	link.addEventListener("click", (e) => {
+		e.preventDefault();
 		navBar.style.transform = 'translateX(-100%)';
-	});
-  })
-// })
+		navBar.style.transition = 'transform 0.5s ease-in-out'
 
+		setTimeout(() => {
+			window.location.href = link.href;
+			}, 500); 
+	});
+});
 
